b16c74eae8d31f61e344b9f581605723548dcde7
chendaile <3185854290@qq.com>
2025-12-08 18:12:50 +0000

    wx framework

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { WordsModule } from './modules/words/words.module'
+import { AuthModule } from './modules/auth/auth.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), WordsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), WordsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
